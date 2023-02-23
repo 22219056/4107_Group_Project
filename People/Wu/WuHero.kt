7926d06c6af86b13101b28559bb409f42a4013d0
Manson Chan <200333307@stu.vtc.edu.hk>
2023-02-23 21:33:38 +0800

    Hero File and hero

---
+++ People/Wu/WuHero.kt
@@ -0,0 +1,91 @@
+package People.Wu
+
+import Card
+import Handler
+import Hero
+import Role
+
+abstract class WuHero(role: Role):Hero(role),Handler{
+    var nation = "Wu"
+    var otherHero: Handler? = null
+
+
+    override fun setNext(h: Handler){
+        otherHero?.setNext(h)
+    }
+
+    override fun handle(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+class SunQuan(role: Role): WuHero(role){
+    override var name = "Sun Quan";
+    override var HP = 4;
+    override var maxHP = 4;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+class GanNing(role: Role): WuHero(role){
+    override var name = "Gan Ning";
+    override var HP = 4;
+    override var maxHP = 4;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+class LuMeng(role: Role): WuHero(role){
+    override var name = "Lu Meng";
+    override var HP = 4;
+    override var maxHP = 4;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+class HuangGai(role: Role): WuHero(role){
+    override var name = "Huang Gai";
+    override var HP = 4;
+    override var maxHP = 4;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+class ZhouYU(role: Role): WuHero(role){
+    override var name = "Zhou YU";
+    override var HP = 3;
+    override var maxHP = 3;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+class LuXun(role: Role): WuHero(role){
+    override var name = "Lu Xun";
+    override var HP = 3;
+    override var maxHP = 3;
+    override var gender = Gender.Male;
+    override var cards = mutableListOf<Card>();
+
+}
+
+
+class DaQiao(role: Role): WuHero(role){
+    override var name = "Da Qiao";
+    override var HP = 3;
+    override var maxHP = 3;
+    override var gender = Gender.Female;
+    override var cards = mutableListOf<Card>();
+}
+
+class SunShangXiang(role: Role): WuHero(role){
+    override var name = "Sun Shang Xiang";
+    override var HP = 3;
+    override var maxHP = 3;
+    override var gender = Gender.Female;
+    override var cards = mutableListOf<Card>();
+}
