8e716067fea43b1f5bfeb1e8dd6640c5179a8d3f
Snowxxd <jacky97260311@gmail.com>
2023-02-27 18:03:24 +0800

    clear

--- People/hero.kt
+++ People/hero.kt
@@ -232,18 +232,3 @@ abstract class Hero(role: Role):Handler {
 
 }
 
-//class LiuBei(role: Role) : Hero(role) {
-//    override var name = "Liu Bei";
-//    override var HP = 4;
-//    override var maxHP = 4;
-//    override var gender = Gender.Male;
-//    override var cards = mutableListOf<Card>();
-//}
-//
-//class GuanYu(role: Role) : Hero(role) {
-//    override var name = "Guan Yu";
-//    override var HP = 4;
-//    override var maxHP = 4;
-//    override var gender = Gender.Male;
-//    override var cards = mutableListOf<Card>();
-//}
